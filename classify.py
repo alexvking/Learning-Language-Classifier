70f835f2ebdcd9f1c18dd2b6e9f5f61a264a04cb
alexvking <alexander.king@tufts.edu>
2014-12-20 12:35:44 -0800

    Python overhaul

---
+++ classify.py
@@ -0,0 +1,99 @@
+# classify.py
+# Language classification ported to Python
+# (c) Alex King, 12/19/14
+
+from __future__ import division # For non-integer division
+import sys # For file IO
+import os  # For directory operations
+import math # For sqrt and bit vector calculation
+
+############################## MODEL GENERATION ################################
+
+# str_to_trigrams : string -> list
+# creates python list of trigrams from string
+def str_to_trigrams(string):
+        list = []
+        for x in range(0,len(string) - 2): # For every group of three letters
+                list.append(string[x] + string[x+1] + string[x + 2])
+        return list
+
+# add_list_to_dict : list, dictionary -> dictionary
+# Adds specified list of trigrams to occurrence dictionary
+def add_list_to_dict(list, dict):
+        for x in list: # For each trigram in the list, add it or increment count
+                if dict.has_key(x):
+                        dict[x] += 1
+                else:
+                        dict[x] = 1
+        return dict
+
+# single_lang_model : string -> tuple (langname, dictionary)
+# creates tuple with specified language name and model (if langname exists)
+def single_lang_model(mode, langname):
+        dict = {}
+        path = "./" + mode + "/" + langname + "/" # construct folder path
+        for file in os.listdir(path):
+                fullpath = path + file
+                trigrams = str_to_trigrams((open(fullpath)).read())
+                add_list_to_dict(trigrams, dict)
+        tuple = (langname, dict);
+        return tuple
+
+# build_all_models : string -> list_of_tuples [(langname, dictionary)]
+# returns list of all model tuples containing language name and model
+# based on mode inputted, lang or subject
+def build_all_models(mode):
+        model_list = []
+        dir = "./" + mode + "/"
+        for lang in os.listdir(dir):
+                model_list.append(single_lang_model(mode, lang))
+        return model_list
+
+def print_model(dict):
+        for keys,values in sorted(dict.items()):
+                sys.stdout.write(keys)
+                sys.stdout.write(": ")
+                print(values)
+
+############################ CLASSIFICATION ####################################
+
+# make_file_model : filename -> dictionary
+# makes trigram occurrence model from specified file (if it exists)
+def make_file_model(filename):
+        trigrams = str_to_trigrams((open(filename)).read())
+        dict = add_list_to_dict(trigrams, {})
+        return dict
+
+# nearest_model : dictionary list_of_tuples -> string
+# returns name of language with highest similarity score given file and models
+def nearest_model(file_model, model_list):
+        score_list = [] # empty score list to start
+        for model in model_list: # Compute each similarity score as list
+                score_list.append(bit_vector_sim(file_model, model))
+
+        # find the index of the highest score, use to index the model list
+        return model_list[score_list.index(max(score_list))][0]
+
+# bit_vector_sim : dictionary dictionary -> number
+# returns bit vector similarity score given two dictionaries; scores range 0-1
+def bit_vector_sim(model1, model2):
+        count = 0
+        for key in model1.keys(): # map over every key
+                if model2[1].has_key(key): 
+                        count += 1
+        return count / ((math.sqrt(len(model1))) * (math.sqrt(len(model2[1]))))
+
+# main
+def main():
+        if len(sys.argv) != 3:
+                print "Usage: python classify.py [--lang|--subject] [file.txt]"
+                sys.exit(1)
+        mode = (str(sys.argv[1]))[2:]
+        if ((mode != "lang") and (mode != "subject")):
+                print "Unrecognized option. Use --lang for natural language," 
+                print "or --subject for academic subject in English."
+                sys.exit(1)
+        file_model = make_file_model(str(sys.argv[2])) # Make model for file
+        model_list = build_all_models(mode) # Make all language models
+        print nearest_model(file_model, model_list) # print language name
+main() # run main
---
+++ readme.txt
@@ -0,0 +1,94 @@
+Alex King
+Simple Language Classifier
+
+SUMMARY
+-------
+
+This is a basic language classifier written in Python, previously created in
+Racket and later C. It relies on a predefined body of "language training" 
+documents to build its reference system.
+
+The classifier works in two main modes: language and subject.
+
+--lang will envoke language mode, which recognizes input text in the following
+languages:
+  - Czech
+  - English
+  - French
+  - German
+  - Hungarian
+  - Italian
+  - Polish
+  - Russian
+  - Spanish
+
+--subject will envoke subject mode, which expects an academic paper (or similar)
+written in English, and will recognize the following subjects of study:
+  - Computer Science
+  - Biology
+  - Psychology
+  - Economics
+
+The comparison is speedy and surprisingly accurate at this point in time. Even
+better, it is under 100 lines of total code.
+
+USAGE
+-----
+
+classify expects plain formatted text specified as a file from the command line.
+
+To run language recognition, run with "python classify.py --lang [input.txt].
+
+To run academic subject recognition, run with --subject instead of --lang.
+
+It is conveneint to run "pdftotext [paper.pdf]" to extract plain text from an
+academic paper formatted as pdf.
+
+ALGORITHM
+---------
+
+classify uses databases of "trigrams" instead of a database of words. This idea
+came from an assignment from Norman Ramsey's COMP 50 class at Tufts University
+in Fall 2013.
+
+Trigrams allow for more granular recognition of roots, prefixes, suffixes, and
+generally discipline-specific terminology. Because of this, classify is able
+to notice patterns and trends and make a guess to what discipline a paper is
+written in.
+
+
+
+KNOWN ISSUES AND PLANNED IMPROVEMENTS
+-------------------------------------
+
+classify currently has no way of knowing if the supplied text is in English for
+the --subject option, so it will silently give wrong answers.
+
+A simple GUI with rich file selection would be a great next step for this
+software. It could also be extended easily to work on programming languages.
+
+More academic subjects should be added soon. There is no obvious way to find a 
+random sampling of various material, so this work is tedious.
+
+VERSION HISTORY AND RELEASE NOTES
+---------------------------------
+
+12/20/14 VERSION 0.5.0
+
+  - Rewritten in Python: a teaching experiment for the author, and an exercise
+    in recognizing what Python is so good at. Python has so many convenient
+    looping and mapping methods built in, it is far easier to manage directories
+    of training documents. The original Racket version was several hundred lines
+    of code, the C version was around 250, and the Python version is under 100.
+  - Returning support of natural language recognition! Run as option --lang. 
+    This was made far easier to implement after porting to Python.
+
+
+11/15/14 VERSION 0.2.0
+
+  - Support for Economics
+  - Initial git commit
+
+11/14/14 VERSION 0.1.0
+  - Initial release
+  - Support for three subjects: Computer Science, Psychology, Biology
